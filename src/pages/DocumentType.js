abd84b0267522d3fb2d3479c0160addc3922dcef
trinhan201 <trinhphieuan@gmail.com>
2023-04-19 15:16:20 +0700

    Document type page #2

--- src/pages/DocumentType.js
+++ src/pages/DocumentType.js
@@ -1,11 +1,34 @@
 import SwitchButton from '~/components/SwitchButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faPlusCircle, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import {
+    faPenToSquare,
+    faPlusCircle,
+    faTrashCan,
+    faSearch,
+    faAngleLeft,
+    faAngleRight,
+} from '@fortawesome/free-solid-svg-icons';
 import DocumentTypeCard from '~/components/Card/DocumentTypeCard';
+import InputField from '~/components/InputField';
 
 const DocumentType = () => {
     return (
         <>
+            <div className="bg-white p-[16px] mb-5 shadow-4Way">
+                <h1 className="text-[1.8rem] md:text-[2.4rem]">Tìm kiếm</h1>
+                <form>
+                    <div className="flex flex-col md:flex-row md:items-center gap-5">
+                        <div className="flex-1">
+                            <InputField placeholder="Tên loại văn bản" />
+                        </div>
+                        <div className="flex-1">
+                            <button className="w-full md:w-fit text-[1.3rem] md:text-[1.6rem] text-[white] bg-[#321fdb] px-[16px] py-[8px] rounded-md hover:bg-[#1b2e4b] transition-all duration-[1s]">
+                                Tìm kiếm <FontAwesomeIcon icon={faSearch} />
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
             <div className="flex items-center justify-between bg-[#f7f7f7] p-[16px] border border-solid border-[#cccccc] mb-[12px] md:mb-0 shadow-4Way">
                 <h1 className="text-[1.8rem] md:text-[2.4rem]">Các loại văn bản</h1>
                 <button className="text-[1.3rem] md:text-[1.6rem] text-[white] bg-[#321fdb] px-[16px] py-[8px] rounded-md hover:bg-[#1b2e4b] transition-all duration-[1s]">
@@ -171,6 +194,15 @@ const DocumentType = () => {
                         </div>
                     </div>
                 </div>
+                <div className="flex items-center justify-end py-3 mr-2">
+                    <p className="text-[1.5rem] mr-9">1-5 of 9</p>
+                    <div className="flex items-center justify-center w-[35px] h-[35px] hover:bg-[#dddddd] rounded-full pointer-events-none opacity-30">
+                        <FontAwesomeIcon icon={faAngleLeft} />
+                    </div>
+                    <div className="flex items-center justify-center w-[35px] h-[35px] hover:bg-[#dddddd] rounded-full">
+                        <FontAwesomeIcon icon={faAngleRight} />
+                    </div>
+                </div>
             </div>
             <div className="md:hidden">
                 <DocumentTypeCard id="1" typeName="Hợp đồng" typeCode="HD" note="Đây là ghi chú" />
@@ -183,6 +215,14 @@ const DocumentType = () => {
                     typeCode="HD"
                     note="Đây là ghi chúĐây là ghi chúĐây là ghi chúĐây là ghi chúĐây là ghi chúĐây là ghi chúĐây là ghi chú"
                 />
+                <div className="flex items-center justify-center">
+                    <div className="bg-[#cccccc] px-[8px] py-[4px] rounded-md mx-1 cursor-pointer hover:bg-[#bbbbbb] pointer-events-none opacity-30">
+                        Prev
+                    </div>
+                    <div className="bg-[#cccccc] px-[8px] py-[4px] rounded-md mx-1 cursor-pointer hover:bg-[#bbbbbb]">
+                        Next
+                    </div>
+                </div>
             </div>
         </>
     );
